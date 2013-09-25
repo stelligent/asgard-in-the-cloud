73f62ff055b0dbbeb1ae77609a1159234af376b9
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-09-25 13:06:55 -0400

    Revert "Merge pull request #2 from ktgit/master"
    This reverts commit 54f925f2a982c6267dddbb83f7bd3230b684cf81, reversing
    changes made to 5b54f210945d3acdaa57b13277c36fe0346ac032.

--- solo.rb
+++ solo.rb
@@ -1,3 +1,2 @@
 file_cache_path "/home/ec2-user/asgard-in-the-cloud/chef/"
 cookbook_path "/home/ec2-user/asgard-in-the-cloud/chef/cookbooks"
-data_bag_path "/home/ec2-user/asgard-in-the-cloud/chef/data_bags"
